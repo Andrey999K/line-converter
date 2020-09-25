19d9b757d318e9994cc4df805fa26283cc7de21e
Andrey999K <andrey1234xxxx@mail.ru>
2020-09-25 18:28:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth / 2;
+canvas.height = innerHeight / 2;
+
+const output = document.createElement("div");
+output.className = "output";
+
+const reset = document.createElement("button");
+reset.className = "button";
+reset.textContent = "Reset";
+
+document.body.appendChild(canvas);
+document.body.appendChild(output);
+document.body.appendChild(reset);
+
+ctx.fillStyle = "orange";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "red";
+let start = false;
+
+canvas.addEventListener("mousedown", () => {
+    if (!start) {
+        ctx.moveTo(event.clientX, event.clientY);
+        output.insertAdjacentText("beforeend", `ctx.moveTo(${event.clientX}, ${event.clientY});`);
+        output.insertAdjacentHTML("beforeEnd", "<br>");
+        start = true;
+    } else {
+        ctx.lineTo(event.clientX, event.clientY);
+        ctx.stroke();
+        output.insertAdjacentText("beforeend", `ctx.lineTo(${event.clientX}, ${event.clientY});`);
+        output.insertAdjacentHTML("beforeEnd", "<br>");
+        // ctx.fill();
+    }
+});
+
+reset.addEventListener("click", () => {
+    start = false;
+    output.textContent = "";
+    ctx.fillStyle = "orange";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
